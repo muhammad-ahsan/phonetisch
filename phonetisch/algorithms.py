5bd0080df825c1995b2e418452e16ec2114bbb9e
Muhammad Ahsan <muhammad.ahsan@mytaxi.com>
2019-04-28 17:40:33 +0200

    Minor refactor

--- phonetisch/algorithms.py
+++ phonetisch/algorithms.py
@@ -64,6 +64,11 @@ class Soundex(PhoneticStrategy):
 
 class Caverphone(PhoneticStrategy):
 
+    def __extract_char_groups(self, word, char):
+        regular_expression = char + '*' + char
+        result = re.search(regular_expression, word)
+        return result.group() if result is not None else None
+
     def __init__(self):
         self.__vowels = ['a', 'e', 'i', 'o', 'u']
 
@@ -80,49 +85,40 @@ class Caverphone(PhoneticStrategy):
             ('ph', 'fh').replace('b', 'p').replace('sh', 's2').replace('z', 's')
 
         self.step_9 = lambda word: word.replace(word[0], 'A') if word[0] in self.__vowels else word
-        # all
-        # other
-        # vowels
-        # with a 3
-        self.step_9_1 = lambda word: word.replace('e', '3').replace('i', '3').replace('o', '3').replace('u', '3')
-        self.step_10 = lambda word: word.replace('3gh3', '3kh3').replace('gh', '22').replace('g', 'k')
+        self.step_10 = lambda word: word.replace('e', '3').replace('i', '3').replace('o', '3').replace('u', '3')
+        self.step_11 = lambda word: word.replace('3gh3', '3kh3').replace('gh', '22').replace('g', 'k')
 
-        self.step_11 = lambda word: word.replace(self.__extract_char_groups(word, 's'),
+        self.step_12 = lambda word: word.replace(self.__extract_char_groups(word, 's'),
                                                  'S') if self.__extract_char_groups(word, 's') is not None else word
-        self.step_12 = lambda word: word.replace(self.__extract_char_groups(word, 't'),
+        self.step_13 = lambda word: word.replace(self.__extract_char_groups(word, 't'),
                                                  'T') if self.__extract_char_groups(word, 't') is not None else word
-        self.step_13 = lambda word: word.replace(self.__extract_char_groups(word, 'p'),
+        self.step_14 = lambda word: word.replace(self.__extract_char_groups(word, 'p'),
                                                  'P') if self.__extract_char_groups(word, 'p') is not None else word
-        self.step_14 = lambda word: word.replace(self.__extract_char_groups(word, 'k'),
+        self.step_15 = lambda word: word.replace(self.__extract_char_groups(word, 'k'),
                                                  'K') if self.__extract_char_groups(word, 'k') is not None else word
-        self.step_15 = lambda word: word.replace(self.__extract_char_groups(word, 'f'),
+        self.step_16 = lambda word: word.replace(self.__extract_char_groups(word, 'f'),
                                                  'F') if self.__extract_char_groups(word, 'f') is not None else word
-        self.step_16 = lambda word: word.replace(self.__extract_char_groups(word, 'm'),
+        self.step_17 = lambda word: word.replace(self.__extract_char_groups(word, 'm'),
                                                  'M') if self.__extract_char_groups(word, 'm') is not None else word
-        self.step_17 = lambda word: word.replace(self.__extract_char_groups(word, 'n'),
+        self.step_18 = lambda word: word.replace(self.__extract_char_groups(word, 'n'),
                                                  'N') if self.__extract_char_groups(word, 'n') is not None else word
 
-        self.step_18 = lambda word: word.replace('w3', 'W3').replace('wy', 'Wy').replace('wh3', 'Wh3').replace \
+        self.step_19 = lambda word: word.replace('w3', 'W3').replace('wy', 'Wy').replace('wh3', 'Wh3').replace \
             ('why', 'Why').replace('w', '2')
-        self.step_19 = lambda word: word.replace(word[0], 'A') if word[0] == 'h' else word
-        self.step_20 = lambda word: word.replace('h', '2').replace('r3', 'R3').replace('ry', 'Ry').replace \
+        self.step_20 = lambda word: word.replace(word[0], 'A') if word[0] == 'h' else word
+        self.step_21 = lambda word: word.replace('h', '2').replace('r3', 'R3').replace('ry', 'Ry').replace \
             ('r', '2').replace('l3', 'L3').replace('ly', 'Ly').replace('I', '2').replace('j', 'y').replace \
             ('y3', 'Y3').replace('y', '2')
-        self.step_21 = lambda word: word.replace('2', '').replace('3', '')
-        self.step_22 = lambda word: (word + '111111')[0:6]
+        self.step_22 = lambda word: word.replace('2', '').replace('3', '')
+        self.step_23 = lambda word: (word + '111111')[0:6]
 
         self.steps = [self.step_1, self.step_2, self.step_3, self.step_4, self.step_5, self.step_6, self.step_7,
-                      self.step_8, self.step_9, self.step_9_1, self.step_10, self.step_11, self.step_12, self.step_13,
-                      self.step_14
-
-            , self.step_15, self.step_16, self.step_17, self.step_18, self.step_19, self.step_20, self.step_21
-            , self.step_22]
+                      self.step_8, self.step_9, self.step_10, self.step_11, self.step_12, self.step_13,
+                      self.step_14, self.step_15, self.step_16, self.step_17, self.step_18, self.step_19, self.step_20,
+                      self.step_21, self.step_22, self.step_23]
 
     def encode_word(self, word):
         pipeline = Pipeline(self.steps)
         return pipeline.execute(word)
 
-    def __extract_char_groups(self, word, char):
-        regular_expression = char + '*' + char
-        result = re.search(regular_expression, word)
-        return result.group() if result is not None else None
+
