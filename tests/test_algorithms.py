694f5bda1b9de7800e64dda01a37d944950ebaf1
dounia sediame <dounia.sediame@gmail.com>
2019-05-11 16:58:44 +0200

    Added tests for Cologne phonetics

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,6 +1,7 @@
 import unittest
 
-from phonetisch import soundex, caverphone
+from phonetisch import soundex, caverphone, cologne_phonetics
+
 
 class TestSoundex(unittest.TestCase):
 
@@ -8,8 +9,14 @@ class TestSoundex(unittest.TestCase):
         assert soundex.encode_word("Example") == soundex.encode_word("Ekzampul")
 
 
-
 class TestCaverphone(unittest.TestCase):
     def test_encode(self):
         assert caverphone.encode_word("Lee") == 'L11111'
         assert caverphone.encode_word("Thompson") == 'TMPSN1'
+
+
+class TestColognePhonetics(unittest.TestCase):
+
+    def test_algorithm(self):
+        word = 'MüllerLüdenscheidt'.upper()
+        assert '65752682' == cologne_phonetics.encode_word(word)
